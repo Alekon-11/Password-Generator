7395a7e239d8e44ef28ebf27fe6b3da5fbe6bde2
Alekon-11 <konyaxin.a@gmail.com>
2024-08-13 15:29:54 +0300

    Made basic styling

---
+++ sctipt.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// const headerTitle = document.querySelector('.header__title');
+
+// function typeTextAnimation(parent) {
+//     let textArr = [...parent.textContent];
+//     let n = 0;
+//     parent.textContent = '';
+
+//     const interval = setInterval(function(){
+//         parent.textContent += textArr[n++];
+//         if(n >= textArr.length){
+//             clearInterval(interval);
+//         }
+//     }, 150);
+   
+// }
+
+// typeTextAnimation(headerTitle);
