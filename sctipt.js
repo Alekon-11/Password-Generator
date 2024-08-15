68fdfb8d2f18ed9da91a0abb0e1cdfa19ffee16e
Alekon-11 <konyaxin.a@gmail.com>
2024-08-15 20:40:36 +0300

    Made display functionality range

--- sctipt.js
+++ sctipt.js
@@ -1,11 +1,28 @@
 'use strict';
 
+const rangeInput = document.querySelector('.range__input');
+const rangeValueBlock = document.querySelector('.range__value');
+
+function chasgeRangeValue(parent, valueContentBlock) {
+    let clientWidth = parent.clientWidth - 30;
+    let leftValue = +parent.value * (clientWidth / +parent.max);
+
+    valueContentBlock.textContent = parent.value;
+    valueContentBlock.style.left = `${leftValue}px`;
+}
+
+rangeInput.addEventListener('input', e => chasgeRangeValue(e.target, rangeValueBlock));
+
+chasgeRangeValue(rangeInput, rangeValueBlock);
+
 // const headerTitle = document.querySelector('.header__title');
 
 // function typeTextAnimation(parent) {
 //     let textArr = [...parent.textContent];
 //     let n = 0;
 //     parent.textContent = '';
+
+//     console.log(textArr);
 
 //     const interval = setInterval(function(){
 //         parent.textContent += textArr[n++];
